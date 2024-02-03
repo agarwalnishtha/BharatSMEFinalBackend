09fa85ca25ebc4fb90eee414a73e838a069e90d4
agarwalnishtha <nishtha861998@gmail.com>
2024-02-04 01:30:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 app.use(bodyParser.json());
 
 // Replace 'YOUR_MONGODB_CONNECTION_STRING' with your actual connection string
